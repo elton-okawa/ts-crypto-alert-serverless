8fb4107ad6dfaf8031be10f3e80935fea46d963f
Elton Okawa <elton.okawa@gmail.com>
2024-05-04 10:22:04 -0300

    feat: add period header

--- src/infra/discord/discord-message.formatter.ts
+++ src/infra/discord/discord-message.formatter.ts
@@ -2,20 +2,52 @@ import {
   INotificationFormatter,
   Notification,
   PercentageNotification,
+  Period,
 } from '@src/domain';
-import { percentage, Regular } from '@src/lib';
+import { Bold, percentage, Regular } from '@src/lib';
 
 export class DiscordMessageFormatter implements INotificationFormatter {
   format(notification: Notification): string {
     return [
       '```ansi',
-      `${Regular.Blue}Percentage:`,
-      ...notification.percentages.map(this.formatPercentage.bind(this)),
+      ...this.formatPercentages(notification.percentages),
       '```',
     ].join('\n');
   }
 
-  private formatPercentage(notification: PercentageNotification) {
+  private formatPercentages(percentages: PercentageNotification[]): string[] {
+    if (percentages.length <= 0) return [];
+
+    const grouped = percentages.reduce(
+      (group, current) => {
+        if (!(current.period in group)) {
+          group[current.period] = [];
+        }
+        group[current.period].push(current);
+
+        return group;
+      },
+      {} as Record<Period, PercentageNotification[]>,
+    );
+
+    return [
+      `${Bold.Blue}=========== Percentage ===========`,
+      ...Object.keys(Period).flatMap((period) => {
+        if (!(period in grouped)) {
+          return [];
+        }
+
+        return [
+          `${Regular.Cyan}${period}:`,
+          ...grouped[period].map((percentage) =>
+            this.formatPercentage(percentage),
+          ),
+        ];
+      }),
+    ];
+  }
+
+  private formatPercentage(notification: PercentageNotification): string {
     const color = this.getColor(notification.difference);
     return `${color}${percentage(notification.difference)} ${notification.symbol.padEnd(4)}: ${notification.targetPrice.format()} -> ${notification.currentPrice.format()}`;
   }
