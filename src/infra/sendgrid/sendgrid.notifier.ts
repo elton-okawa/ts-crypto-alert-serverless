4fe34b9d8da9e1a8e1ffe9ef2534ae9eb51f0e23
Elton Okawa <elton.okawa@gmail.com>
2025-01-11 18:04:43 -0300

    fix: add log message

--- src/infra/sendgrid/sendgrid.notifier.ts
+++ src/infra/sendgrid/sendgrid.notifier.ts
@@ -1,10 +1,13 @@
 import { INotificationFormatter, INotifier, Notification } from '@src/domain';
 import sendgrid from '@sendgrid/mail';
 import { SendgridConfig } from './sendgrid.config';
+import { Logger } from '@src/logger';
 
 export type FormatterResult = Record<string, any>;
 
 export class SendgridNotifier implements INotifier {
+  private readonly logger = new Logger(SendgridNotifier.name);
+
   constructor(
     private _config: SendgridConfig,
     private _formatter: INotificationFormatter<FormatterResult>,
@@ -13,6 +16,8 @@ export class SendgridNotifier implements INotifier {
   }
 
   async send(notification: Notification): Promise<void> {
+    this.logger.log('Sending message...');
+
     const mail = {
       to: this._config.mailTo,
       from: this._config.mailFrom,
@@ -21,5 +26,7 @@ export class SendgridNotifier implements INotifier {
     };
 
     await sendgrid.send(mail);
+
+    this.logger.log('Message sent successfully!');
   }
 }
