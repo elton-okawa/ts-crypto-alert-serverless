e92d0e079f46cc6532d3f48aea0f10aa9dd11126
Elton Okawa <elton.okawa@gmail.com>
2025-01-12 16:52:44 -0300

    fix: alert merge

--- src/send-alert/constants.ts
+++ src/send-alert/constants.ts
@@ -2,7 +2,7 @@ export const BUY_START_COLOR = '#bbff99';
 export const BUY_END_COLOR = '#ecffe3';
 export const SELL_START_COLOR = '#ff9696';
 export const SELL_END_COLOR = '#ffd6d6';
-export const KEEP_COLOR = '#9ca3af';
+export const KEEP_COLOR = '#e5e7eb';
 export const MAX_STREAK = 30;
 export const EMPHASIS_DIFF = {
   diff: 0.2,
